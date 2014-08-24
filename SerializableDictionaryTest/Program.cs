eebf9f027f349cf8d9769ce427290f3cf34d30b7
TakeAsh <take@TakeAsh.net>
2014-08-24 14:32:22 +0900

    rename aliases and variables using MyData1

--- SerializableDictionaryTest/Program.cs
+++ SerializableDictionaryTest/Program.cs
@@ -1,65 +1,65 @@
 ﻿using System;
 using System.ComponentModel;
 
-using ieVV = SerializableDictionaryTest.ImExPorter<int, string>;
-using kvpVV = System.Collections.Generic.KeyValuePair<int, string>;
+using ieVV1 = SerializableDictionaryTest.ImExPorter<int, string>;
+using kvpVV1 = System.Collections.Generic.KeyValuePair<int, string>;
 
-using ieVR = SerializableDictionaryTest.ImExPorter<int, SerializableDictionaryTest.MyData1>;
-using kvpVR = System.Collections.Generic.KeyValuePair<int, SerializableDictionaryTest.MyData1>;
+using ieVR1 = SerializableDictionaryTest.ImExPorter<int, SerializableDictionaryTest.MyData1>;
+using kvpVR1 = System.Collections.Generic.KeyValuePair<int, SerializableDictionaryTest.MyData1>;
 
-using ieRV = SerializableDictionaryTest.ImExPorter<SerializableDictionaryTest.MyData1, int>;
-using kvpRV = System.Collections.Generic.KeyValuePair<SerializableDictionaryTest.MyData1, int>;
+using ieRV1 = SerializableDictionaryTest.ImExPorter<SerializableDictionaryTest.MyData1, int>;
+using kvpRV1 = System.Collections.Generic.KeyValuePair<SerializableDictionaryTest.MyData1, int>;
 
-using ieRR = SerializableDictionaryTest.ImExPorter<SerializableDictionaryTest.MyData1, SerializableDictionaryTest.MyData1>;
-using kvpRR = System.Collections.Generic.KeyValuePair<SerializableDictionaryTest.MyData1, SerializableDictionaryTest.MyData1>;
+using ieRR1 = SerializableDictionaryTest.ImExPorter<SerializableDictionaryTest.MyData1, SerializableDictionaryTest.MyData1>;
+using kvpRR1 = System.Collections.Generic.KeyValuePair<SerializableDictionaryTest.MyData1, SerializableDictionaryTest.MyData1>;
 
 namespace SerializableDictionaryTest {
     class Program {
         static void Main(string[] args) {
-            string filePathVV = @"../Data/SampleVV.log";
-            var dicVV1 = ieVV.create(new kvpVV[]{
-                new kvpVV(0, "Zero"),
-                new kvpVV(1, "One"),
-                new kvpVV(2, "Two"),
+            string filePathVV1 = @"../Data/SampleVV1.log";
+            var dicVV1 = ieVV1.create(new kvpVV1[]{
+                new kvpVV1(0, "Zero"),
+                new kvpVV1(1, "One"),
+                new kvpVV1(2, "Two"),
             });
-            ieVV.export(dicVV1, filePathVV);
-            var dicVV2 = ieVV.import(filePathVV);
+            ieVV1.export(dicVV1, filePathVV1);
+            var dicVV2 = ieVV1.import(filePathVV1);
 
-            string filePathVR = @"../Data/SampleVR.log";
-            var dicVR1 = ieVR.create(new kvpVR[]{
-                new kvpVR(0, new MyData1(){ ID=0, Name="Zero", RegisteredDate=DateTime.Now, Height=170.0, }),
-                new kvpVR(1, new MyData1(){ ID=1, Name="One", RegisteredDate=DateTime.Now, Height=160.0, }),
-                new kvpVR(2, new MyData1(){ ID=2, Name="Two", RegisteredDate=DateTime.Now, Height=165.0, }),
+            string filePathVR1 = @"../Data/SampleVR1.log";
+            var dicVR1 = ieVR1.create(new kvpVR1[]{
+                new kvpVR1(0, new MyData1(){ ID=0, Name="Zero", RegisteredDate=DateTime.Now, Height=170.0, }),
+                new kvpVR1(1, new MyData1(){ ID=1, Name="One", RegisteredDate=DateTime.Now, Height=160.0, }),
+                new kvpVR1(2, new MyData1(){ ID=2, Name="Two", RegisteredDate=DateTime.Now, Height=165.0, }),
             });
-            ieVR.export(dicVR1, filePathVR);
-            var dicVR2 = ieVR.import(filePathVR);
+            ieVR1.export(dicVR1, filePathVR1);
+            var dicVR2 = ieVR1.import(filePathVR1);
 
-            string filePathRV = @"../Data/SampleRV.log";
-            var dicRV1 = ieRV.create(new kvpRV[]{
-                new kvpRV(new MyData1(){ ID=0, Name="Zero", RegisteredDate=DateTime.Now, Height=170.0, }, 0),
-                new kvpRV(new MyData1(){ ID=1, Name="One", RegisteredDate=DateTime.Now, Height=160.0, }, 1),
-                new kvpRV(new MyData1(){ ID=2, Name="Two", RegisteredDate=DateTime.Now, Height=165.0, }, 2),
+            string filePathRV1 = @"../Data/SampleRV1.log";
+            var dicRV1 = ieRV1.create(new kvpRV1[]{
+                new kvpRV1(new MyData1(){ ID=0, Name="Zero", RegisteredDate=DateTime.Now, Height=170.0, }, 0),
+                new kvpRV1(new MyData1(){ ID=1, Name="One", RegisteredDate=DateTime.Now, Height=160.0, }, 1),
+                new kvpRV1(new MyData1(){ ID=2, Name="Two", RegisteredDate=DateTime.Now, Height=165.0, }, 2),
             });
-            ieRV.export(dicRV1, filePathRV);
-            var dicRV2 = ieRV.import(filePathRV);
+            ieRV1.export(dicRV1, filePathRV1);
+            var dicRV2 = ieRV1.import(filePathRV1);
 
-            string filePathRR = @"../Data/SampleRR.log";
-            var dicRR1 = ieRR.create(new kvpRR[]{
-                new kvpRR(
+            string filePathRR1 = @"../Data/SampleRR1.log";
+            var dicRR1 = ieRR1.create(new kvpRR1[]{
+                new kvpRR1(
                     new MyData1(){ ID=0, Name="Zero", RegisteredDate=DateTime.Now, Height=170.0, },
                     new MyData1(){ ID=10, Name="AZero", RegisteredDate=DateTime.Now, Height=175.1, }
                 ),
-                new kvpRR(
+                new kvpRR1(
                     new MyData1(){ ID=1, Name="One", RegisteredDate=DateTime.Now, Height=160.0, },
                     new MyData1(){ ID=11, Name="AOne", RegisteredDate=DateTime.Now, Height=165.1, }
                 ),
-                new kvpRR(
+                new kvpRR1(
                     new MyData1(){ ID=2, Name="Two", RegisteredDate=DateTime.Now, Height=165.0, },
                     new MyData1(){ ID=12, Name="ATwo", RegisteredDate=DateTime.Now, Height=170.1, }
                 ),
             });
-            ieRR.export(dicRR1, filePathRR);
-            var dicRR2 = ieRR.import(filePathRR);
+            ieRR1.export(dicRR1, filePathRR1);
+            var dicRR2 = ieRR1.import(filePathRR1);
 
             TypeConverter myData2Converter = TypeDescriptor.GetConverter(typeof(MyData2));
             MyData2 myData2 = (MyData2)myData2Converter.ConvertFromString(
