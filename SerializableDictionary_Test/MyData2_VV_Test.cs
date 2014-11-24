8d780ae59ecc2aea8ab86c717ad395557737b580
TakeAsh <take@TakeAsh.net>
2014-11-24 10:34:07 +0900

    add MyData2_VV_Test

---
+++ SerializableDictionary_Test/MyData2_VV_Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using TakeAsh;
+
+using ieVV2 = SerializableDictionary_Caller.ImExPorter<double, string>;
+using kvpVV2 = System.Collections.Generic.KeyValuePair<double, string>;
+
+namespace SerializableDictionary_Test {
+
+    [TestFixture]
+    class MyData2_VV_Test {
+
+        private SerializableDictionary<double, string> _dicVV2a;
+
+        private string _dicVV2aXml =
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+            "<SerializableDictionary key_type=\"Double\" value_type=\"String\">\r\n" +
+            "  <item key=\"0.1\" value=\"Zero\" />\r\n" +
+            "  <item key=\"1.2\" value=\"One\" />\r\n" +
+            "  <item key=\"2.3\" value=\"Two\" />\r\n" +
+            "</SerializableDictionary>";
+
+        static string _filePathVV2 = @"../Data/SampleVV2.log";
+
+        [SetUp]
+        public void setup() {
+            _dicVV2a = ieVV2.create(new kvpVV2[]{
+                new kvpVV2(0.1, "Zero"),
+                new kvpVV2(1.2, "One"),
+                new kvpVV2(2.3, "Two"),
+            });
+        }
+
+        [TestCase]
+        public void VV_ToXml() {
+            var actual = _dicVV2a.ToXml();
+            Assert.AreEqual(_dicVV2aXml, actual);
+        }
+
+        [TestCase]
+        public void VV_FromXml() {
+            var actual = SerializableDictionary<double, string>.FromXml(_dicVV2aXml);
+            Assert.AreEqual(_dicVV2aXml, actual.ToXml());
+        }
+
+        [TestCase]
+        public void VV_export() {
+            _dicVV2a.export(_filePathVV2);
+            var actual = "";
+            using (var reader = new StreamReader(_filePathVV2)) {
+                actual = reader.ReadToEnd();
+            }
+            Assert.AreEqual(_dicVV2aXml, actual);
+        }
+
+        [TestCase]
+        public void VV_import() {
+            var actual = ieVV2.import(_filePathVV2);
+            Assert.AreEqual(_dicVV2aXml, actual.ToXml());
+        }
+    }
+}
