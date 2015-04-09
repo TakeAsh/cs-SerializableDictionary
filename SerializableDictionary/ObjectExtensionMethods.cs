5be97c6a377464fae3406bb2f87751e9cbe41905
TakeAsh <take@TakeAsh.net>
2015-04-09 13:16:58 +0900

    add ObjectExtensionMethods (SafeToString())
    - [c# - Checking for null before ToString() - Stack
    Overflow](http://stackoverflow.com/questions/550374/)

---
+++ SerializableDictionary/ObjectExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAsh {
+
+    /// <summary>
+    /// Safe ToString Method
+    /// </summary>
+    /// <remarks>
+    /// [c# - Checking for null before ToString() - Stack Overflow](http://stackoverflow.com/questions/550374/)
+    /// </remarks>
+    public static class ObjectExtensionMethods {
+
+        /// <summary>
+        /// check null before ToString()
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <param name="valueIfNull">value if object is null</param>
+        /// <returns>
+        /// <item><term>object is not null</term><description>object.ToString()</description></item>
+        /// <item><term>object is null</term><description>valueIfNull</description></item>
+        /// </returns>
+        public static string SafeToString(this object obj, string valueIfNull = "") {
+            return obj != null ?
+                obj.ToString() :
+                valueIfNull;
+        }
+    }
+}
---
+++ SerializableDictionary_Test/ObjectExtensionMethods_Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAsh;
+
+namespace SerializableDictionary_Test {
+
+    [TestFixture]
+    class ObjectExtensionMethods_Test {
+
+        public enum Weekdays {
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thu,
+            Fri,
+            Sat,
+        }
+
+        [TestCase(null, null, null)]
+        [TestCase(null, "", "")]
+        [TestCase(null, "undefined", "undefined")]
+        [TestCase(1, null, "1")]
+        [TestCase(1, "", "1")]
+        [TestCase(1, "undefined", "1")]
+        [TestCase(2.2, null, "2.2")]
+        [TestCase(2.2, "", "2.2")]
+        [TestCase(2.2, "undefined", "2.2")]
+        [TestCase(Weekdays.Mon, null, "Mon")]
+        [TestCase(Weekdays.Mon, "", "Mon")]
+        [TestCase(Weekdays.Mon, "Null", "Mon")]
+        public void SafeToString_Test(object obj, string ifNull, string expected) {
+            var actual = obj.SafeToString(ifNull);
+            if (expected != null) {
+                Assert.AreEqual(expected, actual);
+            } else {
+                Assert.IsNull(actual);
+            }
+        }
+    }
+}
