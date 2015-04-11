ded1fba114c4a738a7a05a0d2691de5890b9d7b9
TakeAsh <take@TakeAsh.net>
2015-04-11 18:07:34 +0900

    modify TestCase

--- SerializableDictionary_Test/MyData1_RR_Test.cs
+++ SerializableDictionary_Test/MyData1_RR_Test.cs
@@ -72,7 +72,7 @@ namespace SerializableDictionary_Test {
         [SetUp]
         public void setup() {
             var now = DateTime.Now;
-            _regDate = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            _regDate = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, 0);
             _regDateString = _regDate.ToString("o").Replace(".0000000", "");
 
             _dicRR1a = ieRR1.create(new kvpRR1[]{
