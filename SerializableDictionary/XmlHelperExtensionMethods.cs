d32789f764446e2c97472d8a1114f3a3a77b801d
TakeAsh <take@TakeAsh.net>
2015-04-17 15:58:51 +0900

    add XmlHelperExtensionMethods
    - implement the methods (Import, Export, FromXml, ToXml) for the class
    with IXmlHelper.

---
+++ SerializableDictionary/XmlHelperExtensionMethods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAsh {
+
+    /// <summary>
+    /// Add extension methods (Import, Export, FromXml, ToXml)
+    /// </summary>
+    public interface IXmlHelper { }
+
+    public static class XmlHelperExtensionMethods {
+
+        /// <summary>
+        /// Import object as T from file
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="obj">object (not used)</param>
+        /// <param name="fileName">file name</param>
+        /// <returns>T type object</returns>
+        /// <remarks>
+        /// How to call: (null as T).Import(fileName)
+        /// </remarks>
+        static public T Import<T>(this T obj, string fileName)
+            where T : IXmlHelper {
+
+            return XmlHelper<T>.importFile(fileName);
+        }
+
+        /// <summary>
+        /// Export object to file
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="obj">object</param>
+        /// <param name="fileName">file name</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>true</term><description>success</description></item>
+        /// <item><term>false</term><description>fail</description></item>
+        /// </list>
+        /// </returns>
+        static public bool Export<T>(this T obj, string fileName)
+            where T : IXmlHelper {
+
+            return XmlHelper<T>.exportFile(fileName, obj);
+        }
+
+        /// <summary>
+        /// Convert text to object as T
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="obj">object (not used)</param>
+        /// <param name="text">XML text</param>
+        /// <returns>T type object</returns>
+        /// <remarks>
+        /// How to call: (null as T).FromXml(text)
+        /// </remarks>
+        static public T FromXml<T>(this T obj, string text)
+            where T : IXmlHelper {
+
+            return XmlHelper<T>.convertFromString(text);
+        }
+
+        /// <summary>
+        /// Convert object to XML text
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="obj">object</param>
+        /// <returns>XML text</returns>
+        static public string ToXml<T>(this T obj)
+            where T : IXmlHelper {
+
+            return XmlHelper<T>.convertToString(obj);
+        }
+    }
+}
